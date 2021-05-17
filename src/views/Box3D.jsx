62b1c81d2118d1ddf0390c3d6a6c215857e86358
MalaySatapathy <satapathymaalay.21@gmail.com>
2021-05-18 01:24:54 +0530

    Added plane and box rotation with click/hover effects :boom:

--- src/views/Box3D.jsx
+++ src/views/Box3D.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Stars, Sky } from "@react-three/drei";
 import { Physics, usePlane, useBox } from "@react-three/cannon";
 import "../styles/box3d.css";
 
 function Box(props) {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+  const mesh = useRef();
+  const [state, setState] = useState({ isHovered: false, isActive: false });
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    mesh.current.position.y =
+      mesh.current.position.y + Math.sin(time * 2) / 100;
+    mesh.current.rotation.y = mesh.current.rotation.x += 0.01;
+  });
+
   return (
-    <mesh castShadow>
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={state.isHovered ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={(e) => setState({ ...state, isActive: !state.isActive })}
+      onPointerOver={(e) => setState({ ...state, isHovered: true })}
+      onPointerOut={(e) => setState({ ...state, isHovered: false })}
+      castShadow
+    >
       <boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
-      <meshLambertMaterial attach="material" color="hotpink" />
+      <meshLambertMaterial
+        attach="material"
+        color={state.isActive ? "#820263" : "#D90368"}
+      />
     </mesh>
   );
 }
@@ -26,7 +46,7 @@ function Plane(props) {
       receiveShadow
     >
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <shadowMaterial attach="material" color="lightblue" opacity={0.3} />
+      <meshLambertMaterial attach="material" color="white" />
     </mesh>
   );
 }
@@ -34,33 +54,14 @@ function Plane(props) {
 function Box3D() {
   return (
     <Canvas>
-      <directionalLight
-        castShadow
-        position={[0, 10, 0]}
-        intensity={1.5}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={50}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
-      <pointLight position={[-10, 0, -20]} intensity={0.5} />
-      <pointLight position={[0, -10, 0]} intensity={1.5} />
-      <ambientLight intensity={0.2} />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[2, 2, 2]} angle={0.15} penumbra={1} />
+      <pointLight position={[4, 4, 4]} />
 
       <Physics>
-        <Box />
-
-        <mesh
-          rotation={[-Math.PI / 2, 0, 0]}
-          position={[0, -3, 0]}
-          receiveShadow
-        >
-          <planeBufferGeometry attach="geometry" args={[100, 100]} />
-          <shadowMaterial attach="material" opacity={0.3} />
-        </mesh>
+        <Stars />
+        <Box position={[0, 0, 0]} />
+        <Plane />
       </Physics>
 
       <OrbitControls />
